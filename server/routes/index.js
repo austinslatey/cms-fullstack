45520c998a8bb4675912876f9433af33908f5a16
Austin Slater <austinslater58@gmail.com>
2024-07-01 16:33:14 -0500

    routes/index.js catch all statement moved above the api router.use() it seemed as if render was thinking this was an actual route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
-
 router.use((req, res) => {
   return res.send('Wrong route!');
 });
+
+router.use('/api', apiRoutes);
 
 module.exports = router;
