ca1a920a9818b8f0339cbc5d979fd14fbdd6874e
Austin Slater <austinslater58@gmail.com>
2024-07-07 08:45:47 -0500

    Fixed invalid date

--- client/src/components/pages/Profile/Profile.jsx
+++ client/src/components/pages/Profile/Profile.jsx
@@ -155,7 +155,7 @@ export default function Profile() {
           </div>
           <h2 className="border border-secondary m-2 p-4 rounded text-center">{thought.thoughtTitle}</h2>
           <p className="border border-secondary m-2 p-4 rounded text-center">{thought.thoughtText}</p>
-          <p className="text-light text-center m-4">{new Date(thought.createdAt).toLocaleString()}</p>
+          <p className="text-light text-center m-4">{thought.createdAt}</p>
           <div className="d-flex justify-content-center my-2">
             <button className="btn btn-primary m-2">Comment</button>
             <button className="btn btn-primary m-2">Like this</button>
