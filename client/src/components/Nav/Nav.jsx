49fc55111603f61e48038dc1935cbc963ad191e7
Austin Slater <austinslater58@gmail.com>
2024-07-21 22:04:57 -0500

    Nav conditionally renders -> if user has custom profile picture, it renders on Nav bar, else renders default

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,63 +1,70 @@
 import { NavLink } from "react-router-dom";
+import { Buffer } from 'buffer';
 import homeImg from "../../assets/home.png";
 import defaultImg from "../../assets/avi.png";
 import '../Header/Header.css'
 
-import { useEffect, useState } from 'react'
-import { useAppContext } from "../../providers/AppProvider"
-import Cookie from "js-cookie"
+import { useEffect, useState } from 'react';
+import { useAppContext } from "../../providers/AppProvider";
+import Cookie from "js-cookie";
 
-
-export default function Nav() {
-  const [loggedIn, setLoggedIn] = useState(false)
+export default function Nav({ avatar = {} }) {
+  const [loggedIn, setLoggedIn] = useState(false);
   const { currentUser } = useAppContext();
+
   function logout() {
-    Cookie.remove('auth-cookie')
-    window.location.href = '/'
+    Cookie.remove('auth-cookie');
+    window.location.href = '/';
   }
 
-
-  // json.parse session storage
   useEffect(() => {
-    // console.log(currentUser)
-    currentUser && setLoggedIn(true)
-  }, [currentUser])
-
+    if (currentUser) {
+      setLoggedIn(true);
+    }
+  }, [currentUser]);
 
   return (
     <nav>
       {/* Home tab */}
-      <NavLink className="navbar" to="/"
-        style={({ isActive }) =>
-        ({ textDecoration: isActive ? 'underline' : 'none' }
-        )}><img src={homeImg} alt="home" className="rounded nav-avatar"/></NavLink>
-      <NavLink className="navbar" to="/about"
-        style={({ isActive }) =>
-        ({ textDecoration: isActive ? 'underline' : 'none' }
-        )}><img src={defaultImg} alt="avi" className="rounded nav-avatar"/></NavLink>
+      <NavLink className="navbar" to="/" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+        <img src={homeImg} alt="home" className="rounded nav-avatar" />
+      </NavLink>
 
-      {/* if else for displaying sign up/login tab */}
-      {(!loggedIn) ? (
+      {/* Profile tab with avatar */}
+      <NavLink className="navbar" to="/about" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+        {avatar.data && avatar.contentType ? (
+          <img
+            src={`data:${avatar.contentType};base64,${Buffer.from(avatar.data).toString('base64')}`}
+            alt="Profile"
+            className="rounded nav-avatar"
+          />
+        ) : (
+          <img
+            src={defaultImg}
+            alt="Default Profile"
+            className="rounded nav-avatar"
+          />
+        )}
+      </NavLink>
+
+      {/* Login/Signup or Account/Logout tabs */}
+      {!loggedIn ? (
         <>
-          {/* Signup tab */}
-          <NavLink className="navbar" to="/login" style={({ isActive }) =>
-          ({ textDecoration: isActive ? 'underline' : 'none' }
-          )}>Login</NavLink>
-          <NavLink className="navbar" to="/signup" style={({ isActive }) =>
-          ({ textDecoration: isActive ? 'underline' : 'none' }
-          )}>Sign up</NavLink>
-
+          <NavLink className="navbar" to="/login" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Login
+          </NavLink>
+          <NavLink className="navbar" to="/signup" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Sign up
+          </NavLink>
         </>
       ) : (
         <>
-          {/* Profile tab */}
-          <NavLink className="navbar" to="/usersettings" style={({ isActive }) =>
-          ({ textDecoration: isActive ? 'underline' : 'none' }
-          )}>Your Account</NavLink>
-          {/* Logout button */}
+          <NavLink className="navbar" to="/usersettings" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Your Account
+          </NavLink>
           <span className="logout" onClick={logout}>Logout</span>
         </>
       )}
-    </nav >
-  )
+    </nav>
+  );
 }
